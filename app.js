d148072707b55157bd8f4d171b1a87f4654d802b
Sarah LaFleur <lafleurpiano@gmail.com>
2018-04-05 19:07:40 -0500

    Said "hello" to myself

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', apiRoutes);
-
+//hi sarah
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
